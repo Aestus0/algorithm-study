7c74424d7c41bf172c00f49dcbb270f3ad32678b
Aestus0 <35069074+Aestus0@users.noreply.github.com>
2021-08-10 19:54:38 +0800

    Create 41.缺失的第一个正数.js

---
+++ 41.缺失的第一个正数.js
@@ -0,0 +1,64 @@
+/**
+给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ 
+请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ 
+
+示例 1：
+
+输入：nums = [1,2,0]
+输出：3
+示例 2：
+
+输入：nums = [3,4,-1,1]
+输出：2
+示例 3：
+
+输入：nums = [7,8,9,11,12]
+输出：1
+*/
+
+var getValue = function (value) {
+  if (value < 0) {
+    return -value;
+  }
+  return value;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] <= 0) {
+      nums[i] = nums.length + 1;
+    }
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    var value = getValue(nums[i]);
+    if (value >= 1 && value <= nums.length && nums[value - 1] > 0) {
+      nums[value - 1] = -nums[value - 1];
+    }
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      return i + 1;
+    }
+  }
+  return nums.length + 1;
+};
+
+
+/**
+ * 根据hash表查询改造而来
+ * 
+ * 假设数组长度是n
+ * 
+ * 首先得到的正数只可能在1到n+1之间，那么小于等于0和大于n的数就不对结果产生影响。
+ * 于是我们将所有小于0的数都转换成n+1
+ * 再遍历数组，取第i位数的绝对值，如果绝对值|nums[i]|大于等于1小于等于n则将nums[|nums[i]|-1]的值变成负数记录下来
+ * 最后遍历数组，找到第i位数大于0，则结果位i+1;否则结果位n+1;
+ * /
